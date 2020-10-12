a855ec54dbd0dd6189f5a9141586cdf002d713dc
the-uniq-sam <samyakjain455@gmail.com>
2020-10-12 19:17:22 +0530

    Updated Events

--- Sports_Management_System/Events/models.py
+++ Sports_Management_System/Events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class event(models.Model):
+    name = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=False)
+    organiser = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'events'
+    
