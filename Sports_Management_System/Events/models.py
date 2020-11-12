b55c5803866dfb3ea846907065b6187413cd9b02
the-uniq-sam <samyakjain455@gmail.com>
2020-11-12 23:20:47 +0530

    Final for today

--- Sports_Management_System/Events/models.py
+++ Sports_Management_System/Events/models.py
@@ -5,7 +5,7 @@ class event(models.Model):
     name = models.CharField(max_length=50)
     timestamp = models.DateTimeField(auto_now_add=False)
     organiser = models.CharField(max_length=100)
-    about = models.TexField(default='')
+    about = models.TextField(default='')
     rules = models.TextField(default='')
     temp = 'csvfile_for_guest/' + str(id) + '/'
     csvfile = models.FileField(upload_to=temp)
--- Sports_Management_System/Events/urls.py
+++ Sports_Management_System/Events/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 urlpatterns = [
