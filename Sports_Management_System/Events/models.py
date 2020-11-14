4f12b5ff9a6a1ddb8ea44ecc768f9f370e2313de
Meliodas-dragon-wrath <sachin@DESKTOP-P2TL4RT.localdomain>
2020-11-14 13:06:43 +0530

    rules Updated

--- Sports_Management_System/Events/models.py
+++ Sports_Management_System/Events/models.py
@@ -16,20 +16,36 @@ class event(models.Model):
         verbose_name_plural = 'events'
 
 
-class rule(models.Model,event):
-    event_id = models.IntegerField()
-    rule1 = models.TextField(default='')
-    rule2 = models.TextField(default='')
-    rule3 = models.TextField(default='')
-    rule4 = models.TextField(default='')
-    rule5 = models.TextField(default='')
+# class rule(models.Model):
+#     id= models.OneToOneField(
+#     event,
+#     on_delete=models.CASCADE,
+#     )
+#     event_id = models.IntegerField()
+#     rule1 = models.TextField(default='')
+#     rule2 = models.TextField(default='')
+#     rule3 = models.TextField(default='')
+#     rule4 = models.TextField(default='')
+#     rule5 = models.TextField(default='')
+#
+    # def __str__(self):
+        # return self.
 
-    def __str__(event):
-        return event.name
-      
+
+class rule(models.Model):
+    event_id = models.IntegerField()
+    event_name = models.CharField(max_length=50,default='')
+    rule1 = models.TextField(default='')
+    rule2 = models.TextField(default='',blank=True)
+    rule3 = models.TextField(default='',blank=True)
+    rule4 = models.TextField(default='',blank=True)
+    rule5 = models.TextField(default='',blank=True)
+
+    def __str__(self):
+        return str(self.event_id)
+
 class guest(models.Model):
     id_for_event = models.IntegerField()
     name = models.CharField(max_length=100)
     motto = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
-    
